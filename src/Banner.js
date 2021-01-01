b59bc1b0669f1de4a5f3c97caa71dab45720d2b4
asep10001 <asep10001@mail.unpad.ac.id>
2021-01-01 15:07:18 +0700

    done wwith banner

--- src/Banner.js
+++ src/Banner.js
@@ -39,14 +39,15 @@ function Banner() {
           {movie?.name || movie?.title || movie?.original_name}
         </h1>
         <div className="banner__buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List</button>
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
           {truncate(movie?.overview, 150)}
         </h1>
         {/* description */}
       </div>
+      <div className="banner--fadeBottom"/>
     </header>
   );
 }
